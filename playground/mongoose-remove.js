9230e2968d72b5dd9335546a2211132d7f76e3a8
Xianchu <xxc141730@gmail.com>
2017-11-04 20:04:04 -0500

    Add DELETE /todos/:id

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,13 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+Todo.findByIdAndRemove('59fe5f1d9fb00e52adff0f65').then((todo) => {
+  console.log(todo);
+});
